d7800c5818349b7a3f686c968bbf997cd9f1618b
Matheus Alexandre <matheus.oliveiralx@gmail.com>
2022-09-03 09:26:29 -0300

    Update awg.py

--- awg.py
+++ awg.py
@@ -1,2 +1,11 @@
-import matplotlib.pyplot as plt
-import numpy as np
+class AwgCalc:
+    def __init__(self, awg):
+        self.number = awg
+
+    def set_awg(self, awg):
+        self.awg = awg
+
+    def get_resist(self):
+        
+
+        return r
