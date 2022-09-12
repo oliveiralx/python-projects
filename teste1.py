8e4081035becd8d79c671bcab9ac0240e5c4123e
Matheus Alexandre <matheus.oliveiralx@gmail.com>
2022-09-12 20:48:47 -0300

    default

--- teste1.py
+++ teste1.py
@@ -1,5 +1,7 @@
-#import pandas as np
+import pandas as np
 import readingdata as rd
 
-t1 = rd.leiaTexto('/readingdata/TESTE1.txt', delimiter='|')
-t2 = rd.leiaTexto('/readingdata/TESTE2.txt', delimiter='\t')
+#t1 = rd.leiaTexto('/readingdata/TESTE1.txt', delimiter='|')
+#t2 = rd.leiaTexto('/readingdata/TESTE2.txt', delimiter='\t')
+
+txt = np.read_csv('/readingdata/TESTE2.txt', delimiter='\t')
