bd5eb4b0834bab7eaff62f64344b34b91d6e3403
Matheus Alexandre <matheus.oliveiralx@gmail.com>
2022-09-16 23:52:10 -0300

    Criação do Pyserial

---
+++ pyserial.py
@@ -0,0 +1,14 @@
+import serial.tools.list_ports
+
+ports = serial.tools.list_ports.comports()
+serialInst = serial.Serial()
+
+portList = []
+
+for onePort in ports:
+    portList.append(str(onePort))
+    print(str(onePort))
+
+val = input("Select Port: COM")
+
+print(f'COM{var}')
