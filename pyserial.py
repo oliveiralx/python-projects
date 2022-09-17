e47af6807118b24f28a7a238b927ba72a4b7cc56
Matheus Alexandre <matheus.oliveiralx@gmail.com>
2022-09-17 00:55:54 -0300

    some sugestions

--- pyserial.py
+++ pyserial.py
@@ -14,9 +14,15 @@ val = input("Select Port: COM")
 for x in range(0, len(portList)):
     if portList[x].startswith("COM" + str(val)):
         portVar = "COM" + str(val)
-        print(portList[x] + " Selecionada")
+        print(portList[x] + " Conected")
     else:
-        print("Invalid COM")
+        print("Error")
+
+"""
+    A sugestion is find a way to better make sure if the user choose 
+    a wrong COM the program will return and ask again for the user 
+    about the right COM. The changes can be in the lines 14-19.
+"""
 
 serialInst.baudrate = 115200
 serialInst.port = portVar
@@ -26,3 +32,12 @@ while True:
     if serialInst.in_waiting:
         packet = serialInst.readline()
         print(packet.decode('utf').rstrip('\n'))
+
+"""
+    Now in the lines 31-34, we can change about how to stop the
+    program. Or we can do something to read a number of datas that
+    need to be colected and then, finish the program.
+
+    I prefer specify a comand to stop, but for read a number we
+    can integrate the code with a GUI to make the code mor friendlier.
+"""
