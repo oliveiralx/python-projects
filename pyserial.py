1fbad1292908706f139ed8f228535efd1fa877d1
Matheus Alexandre <matheus.oliveiralx@gmail.com>
2022-10-26 23:00:27 -0300

    options to stop the while

--- pyserial.py
+++ pyserial.py
@@ -1,5 +1,5 @@
 import serial.tools.list_ports
-import time
+import time import time
 
 ports = serial.tools.list_ports.comports()
 serialInst = serial.Serial()
@@ -20,32 +20,17 @@ for x in range(0, len(portList)):
         print(f'Colecting data from {portVar}...\n')
     else:
         print('Access Denied')
-
-"""
- -> A sugestion is find a way to better make sure if the user choose 
-    a wrong COM the program will return and ask again for the user 
-    about the right COM. The changes can be in the lines 14-19.
-
- -> Maybe that'll be possible to solve the problem using a while condition.
-    If while the user don't choose a available COM, the program
-    will return to the begin and ask again for the user.
-    
-"""
-
+        
 serialInst.baudrate = 115200
 serialInst.port = portVar
 serialInst.open()
+
+t_wait = 10
+
+RPM = []
 
 while True:
     if serialInst.in_waiting:
         packet = serialInst.readline()
         print(packet.decode('utf').rstrip('\n'))
-
-"""
- -> Now in the lines 31-34, we can change about how to stop the
-    program. Or we can do something to read a number of datas that
-    need to be colected and then, finish the program.
-
- -> I prefer specify a comand to stop, but for read a number we
-    can integrate the code with a GUI to make the code mor friendlier.
-"""
+        RPM.append(packet)
